eb48b1b031a9def81e4b5be668353ed0a32d0782
DeepuNarayana <dpu7481@gmail.com>
2026-08-10 16:46:51 +0530

    Application is ready for deployement

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -10,15 +12,20 @@ app = FastAPI(
     version="1.0.0",
 )
 
+
+frontend_url = os.getenv(
+    "FRONTEND_URL",
+    "http://localhost:5173",
+)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-    ],
+    allow_origins=[frontend_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/")
 def root():
